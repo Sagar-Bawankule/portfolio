17743181985e9d35694fff746152fb69bb9318a4
Your Name <sagarbawankule335@gmail.com>
2025-07-27 17:34:35 +0530

    update json file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Enable static export
+  output: 'export',
+  
   // Remove telemetry to fix warning
   // telemetry: false, // This line was causing the warning
   
@@ -9,6 +12,7 @@ const nextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 60,
+    unoptimized: true, // Required for static export
   },
   
   // Performance optimizations
